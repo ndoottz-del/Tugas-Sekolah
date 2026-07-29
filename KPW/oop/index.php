b0805a31d99b534a4f3a9aff00e3ef5711321aec
Rendyy RPL 2 <ndoottz@gmail.com>
2026-07-29 19:02:27 +0700

    Add files via upload

--- KPW/oop/index.php
+++ KPW/oop/index.php
@@ -1,1 +1,24 @@
+<?php
 
+require_once("animal.php");
+require_once("Frog.php");
+require_once("Ape.php");
+
+$sheep = new Animal("shaun");
+echo "Name : " . $sheep->name . "<br>";
+echo "legs : " . $sheep->legs . "<br>";
+echo "cold blooded : " . $sheep->cold_blooded . "<br><br>";
+
+$kodok = new Frog("buduk");
+echo "Name : " . $kodok->name . "<br>";
+echo "legs : " . $kodok->legs . "<br>";
+echo "cold blooded : " . $kodok->cold_blooded . "<br>";
+$kodok->jump();
+echo "<br>";
+
+$sungokong = new Ape("kera sakti");
+echo "Name : " . $sungokong->name . "<br>";
+echo "legs : " . $sungokong->legs . "<br>";
+echo "cold blooded : " . $sungokong->cold_blooded . "<br>";
+$sungokong->yell();
+?>
